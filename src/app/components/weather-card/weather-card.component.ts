164789b35a0ef2af835347ff5175b6751039953e
IKupreev <igor.kupreev88@gmail.com>
2023-11-15 23:09:14 +0700

    add weather-card and add-card components

---
+++ src/app/components/weather-card/weather-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-weather-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './weather-card.component.html',
+  styleUrl: './weather-card.component.scss'
+})
+export class WeatherCardComponent {
+
+}
