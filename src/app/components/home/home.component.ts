571b5045f95038ab4e1bca702442a3259492a213
IKupreev <igor.kupreev88@gmail.com>
2023-11-16 16:24:54 +0700

    add routing

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { WeatherCardComponent } from '../weather-card/weather-card.component';
+import { AddCardComponent } from '../add-card/add-card.component';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, WeatherCardComponent, AddCardComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
