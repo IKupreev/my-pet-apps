b76b0522e7166736aefc9bf9bc34c974b0624008
IKupreev <igor.kupreev88@gmail.com>
2023-11-24 14:59:55 +0700

    create new branch portfolio  and added components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/portfolio/header/header.component';
+import { SideBarComponent } from './components/portfolio/side-bar/side-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, SideBarComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './components/portfolio/pages/about/about.component';
+import { SkillsComponent } from './components/portfolio/pages/skills/skills.component';
+import { PortfolioComponent } from './components/portfolio/pages/portfolio/portfolio.component';
+import { NotFoundComponent } from './components/portfolio/pages/not-found/not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: AboutComponent, title: 'About Page' },
+  { path: 'skills', component: SkillsComponent, title: 'Skills Page' },
+  { path: 'portfolio', component: PortfolioComponent, title: 'Portfolio Page' },
+  { path: '**', component: NotFoundComponent },
+];
