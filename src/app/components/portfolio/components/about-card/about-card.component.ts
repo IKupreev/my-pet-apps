d52491c116d1205bbb7026b5cb49b10526d25d80
IKupreev <igor.kupreev88@gmail.com>
2023-11-26 23:21:16 +0700

    added new components, about and quallification cards

---
+++ src/app/components/portfolio/components/about-card/about-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-about-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './about-card.component.html',
+  styleUrl: './about-card.component.scss'
+})
+export class AboutCardComponent {
+
+}
---
+++ src/app/components/portfolio/components/qualification-card/qualification-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-qualification-card',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './qualification-card.component.html',
+  styleUrl: './qualification-card.component.scss'
+})
+export class QualificationCardComponent {
+
+}
