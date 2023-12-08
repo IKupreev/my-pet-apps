424c0cee58dafa08d190a1c015500bd8ceab6e96
IKupreev <igor.kupreev88@gmail.com>
2023-12-08 16:23:23 +0700

    Added expirience block

---
+++ src/app/components/portfolio/components/Expirience-Block/Expirience-Block.component.ts
@@ -0,0 +1,18 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-Expirience-Block',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './Expirience-Block.component.html',
+  styleUrls: ['./Expirience-Block.component.css']
+})
+export class ExpirienceBlockComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
